79d1ae1ebd4a926c1317fddfec29c878e5e7de8f
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-03-03 21:03:05 +0800

    Update AccountState.java

--- AccountState.java
+++ AccountState.java
@@ -1,1 +1,7 @@
-
+public interface AccountState {
+    public void deposit(double depositAmount, Account account);
+    public void withdraw(double withdrawAmount, Account account);
+    public void activate(Account account);
+    public void suspend(Account account);
+    public void close(Account account);
+}
